9e6bcc390f4675c2d9d22503e279c82a70a5c298
Nicholas <nicholasnguyen1129@gmail.com>
2025-03-19 20:42:26 -0700

    feat(project): Add activity and Add journal entry button do stuff now

--- app/screens/ActivitiesScreen.tsx
+++ app/screens/ActivitiesScreen.tsx
@@ -1,11 +1,33 @@
-import React from 'react';
-import { View, Text, Button } from 'react-native';
+import React, {useState} from 'react';
+import { View, Text, Button, TextInput } from 'react-native';
 
+const addActivity = () => { 
+  console.log('Adding new activity...');
+  return (
+    <View>
+      <Text>New Activity</Text>
+      <TextInput placeholder="Activity Name" />
+      <TextInput placeholder="Activity Type" />
+      <TextInput placeholder="Activity Duration" />
+      <Button title="Save Activity" onPress={() => {
+        console.log('Activity saved!');
+      }} />
+    </View>
+  );
+}
 const ActivitiesScreen = () => {
+  const [activity, setActivity] = useState(false);
   return (
     <View>
       <Text>Activities Screen</Text>
-      <Button title="Add Activity" onPress={() => {}} />
+      <Button title="Add Activity" onPress={() => {
+        setActivity(true);
+      }} />
+      {activity && <TextInput placeholder="Activity Name" underlineColorAndroid={"black"}/>}
+      {activity && <TextInput placeholder="Activity Duration" underlineColorAndroid={"black"} />}
+      {activity && <Button title="Save Activity" onPress={() => {
+        setActivity(false);
+      }} />}
     </View>
   );
 };
--- app/screens/JournalScreen.tsx
+++ app/screens/JournalScreen.tsx
@@ -1,11 +1,18 @@
-import React from 'react';
-import { View, Text, Button } from 'react-native';
+import React, {useState} from 'react';
+import { View, Text, Button, TextInput } from 'react-native';
 
 const JournalScreen = () => {
+  const [journal, setJournal] = useState(false);
   return (
     <View>
       <Text>Journal Screen</Text>
-      <Button title="Add Journal Entry" onPress={() => {}} />
+      <Button title="Add Journal Entry" onPress={() => {
+        setJournal(true);
+      }} />
+      {journal && <TextInput placeholder="Journal Entry" underlineColorAndroid={"black"} multiline numberOfLines={5}/>}
+      {journal && <Button title="Save Entry" onPress={() => {
+        setJournal(false);
+      }} />}
     </View>
   );
 };
